e9cbc306691bf22a077f0e74c0b84864ba1b80c5
kn52 <ashish52922@gmail.com>
2020-08-31 09:48:27 +0530

    [Aashish]ADDED:Css file for add button

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -3,15 +3,14 @@ import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Add from '../images/svg/Add.svg'
+import '../css/DashBoard.css'
 
 export default function DashBoard () {
 	return (
 	<Grid container spacing = {2} xs = {12} >
-		<Grid item xs = {10} style = {{display:"flex",justifyContent:"space-between", border:"1px solid white", width:"300px"}}>
-			<Button variant = "contained" color = "primary" style = {{fontSize:"20px",fontFamily:"Cambria",fontWeight:"bold",width:"250px", height:'50px'}} 
-			startIcon={<img src={Add} alt="" width="30px" height="30px"/>} >Add Employee</Button>
-			<Button variant = "contained" color = "primary" style = {{display:"none", fontSize:"20px",fontFamily:"Cambria",fontWeight:"bold",width:"150px", height:'50px'}} >
-				Login</Button>
+		<Grid item xs = {10} className='add_employee'>
+			<Button variant = "contained" color = "primary" 
+			startIcon={<img src={Add} alt="" className='add_image_icon'/>} className='add_employee-button font_family'>Add Employee</Button>
 		</Grid>
 	</Grid>
 	);
