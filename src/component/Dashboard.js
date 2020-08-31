a848b3d71f95ebf2d0f5e5a55be30cf972ccf1c6
kn52 <ashish52922@gmail.com>
2020-08-31 18:28:45 +0530

    [Aashish]ADDED:Css for app bar and icon for greeting app

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -2,21 +2,26 @@ import React from 'react';
 import '../css/DashBoard.css';
 import '../css/Employee.css';
 import Grid from '@material-ui/core/Grid';
-import DisplayButton from '../utility/DisplayButton';
 import DisplayData from './DisplayData';
+import DisplayAppBar from '../utility/DisplayAppBar';
+import DisplayButton from '../utility/DisplayButton';
 import Add from '../images/svg/Add.svg'
 
 export default function DashBoard () {
 	return (
-		<Grid container spacing = {2} xs = {12} >
-			<Grid item xs = {10} className='add_employee'>
+		<Grid container spacing = {0} xs = {12} >
+			<Grid item xs = {12} className='add_employee'>
+				<DisplayAppBar />
+			</Grid>
+			<Grid item xs = {12} className='employee'></Grid>
+			<Grid item xs = {12} className='add_employee'>
 				<DisplayButton imag={Add} imageclass='image_icon'  buttonclass='add_employee_button font_family' txt='Add Employee' />
 			</Grid>
-			<Grid item xs = {10} className='add_employee'>
-				<DisplayButton imag={Add} imageclass='image_icon'  buttonclass='add_employee_button font_family' txt='Add Employee' />
-			</Grid>
-			<Grid item xs = {10} className='add_employee'>
-				<DisplayData />
+			<Grid item xs = {12} className='employee'></Grid>
+			<Grid item xs = {12} className='add_employee'>
+				<Grid item xs = {10} className='add_employee'>
+					<DisplayData />
+				</Grid>
 			</Grid>
 		</Grid>
 	);
--- src/utility/DisplayAppBar.js
+++ src/utility/DisplayAppBar.js
@@ -1,24 +1,40 @@
 import React from 'react';
+import '../css/Employee.css';
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Greeting from '../images/svg/GreetingApp.svg';
 
 const useStyles = makeStyles((theme) => ({
-	root: {
-	  flexGrow: 1,
-	},
-	
-	title: {
-	  flexGrow: 1,
-	},
+    root: {
+      flexGrow: 1,
+      paddingLeft:5,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+      fontSize:30,
+      fontFamily: 'cambria',
+      fontWeight: 'bold',
+    },
   }));
 
 export default function DisplayAppBar () {
+ 
+    const classes = useStyles();
+
     return (
         <>
             <AppBar position="static">
                 <Toolbar>
-                    <Typography variant="h6" className={classes.title}>
+                    <IconButton edge="start"  color="inherit" aria-label="menu">
+                        <img src={Greeting} className='greeting_image_icon' alt='' />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}  >
                         Greeting App
                     </Typography>
                 </Toolbar>
