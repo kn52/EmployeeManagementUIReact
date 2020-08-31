17c67c8d1891c26c5f0396586b9c74126724e776
kn52 <ashish52922@gmail.com>
2020-08-31 09:34:34 +0530

    [Aashish]ADDED:Svg image in separate folder

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -2,22 +2,9 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import Add from './Add.svg'
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+import Add from '../images/svg/Add.svg'
 
 export default function DashBoard () {
-	const classes = useStyles();
 	return (
 	<Grid container spacing = {2} xs = {12} >
 		<Grid item xs = {10} style = {{display:"flex",justifyContent:"space-between", border:"1px solid white", width:"300px"}}>
