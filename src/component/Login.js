f6e20c91b85f8f55047dad886dd4457fd923161c
kn52 <ashish52922@gmail.com>
2020-09-01 19:20:58 +0530

    [Aashish]ADDED:Form to log in by employee

---
+++ src/component/Login.js
@@ -0,0 +1,64 @@
+import React, {useState} from 'react';
+import '../css/Login.css';
+import DisplayAppBar from '../utility/DisplayAppBar';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+export default function Login () {
+
+    const initialState = { 
+        email:'',
+        password:'',
+    }
+
+    const [{email,password},setValues]=useState(initialState);
+
+    const handleValues = (event) => {
+        event.persist();
+        setValues(values=>({...values,[event.target.name]: event.target.value}));
+    }
+
+    const handleSave = () => {
+        setValues({ ...initialState });
+    }
+
+    const handleCancel = () => { setValues({ ...initialState }); }
+    
+    return (
+        <>
+            <div className="login_container">
+                <div className="login_title_container">
+                    <DisplayAppBar title='Greeting App'/>
+                </div>
+                <div className="login_space"></div>
+                <div className="login_child">
+                    <div className="login_content">
+                        <h1><span className="login_title">Login</span></h1>
+                    </div>
+
+                    <form id="baseForm" onSubmit={handleSave} onReset={handleCancel}>
+                        <div className="login_content">
+                            <TextField name="email" label="Email" variant="outlined" value={email}
+                                onChange={handleValues} size="small" required />
+                        </div>
+
+                        <div className="login_bar_space"></div>
+
+                        <div className="login_content">
+                            <TextField name="password" label="Password" variant="outlined" value={password}
+                                onChange={handleValues} size="small" required />
+                        </div>
+
+                        <div className="login_space"></div>
+                            
+                        <div className="login_content">
+                            <Button type="submit" className="login_button" >
+                                Login
+                            </Button>
+                        </div>     
+                    </form>
+                </div>
+            </div>
+        </>
+    );
+}
